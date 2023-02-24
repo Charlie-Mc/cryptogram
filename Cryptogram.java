f4c88652df2118644b569ff653909de783f66793
conno <connorpodesta@gmail.com>
2023-02-24 18:47:59 +0000

    generate function in Cryptogram.java

--- Cryptogram.java
+++ Cryptogram.java
@@ -1,3 +1,8 @@
+/*
+TODO:
+    This is for the generate Branch, delete this comment when complete
+ */
+
 public class Cryptogram {
 
     public void start () {
@@ -5,7 +10,7 @@ public class Cryptogram {
         //TODO: finish this function, starts the cryptogram and will call to the gui to swap to game
     }
 
-    public void genarate (int Size) {
+    public void generate (int Size) {
         Encryption encryption = new Encryption();
         //TODO: create cryptogram generator
     }
