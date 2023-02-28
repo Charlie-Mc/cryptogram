28c3332ecfe75342b79f017fdc61a51390b8cc48
Adam <adam.dunbar.2021@uni.strath.ac.uk>
2023-02-28 17:32:34 +0000

    Modified and populated Player class

--- Player.java
+++ Player.java
@@ -1,2 +1,41 @@
 public class Player {
+    String userName;
+    int userScore;
+    int correctGuesses;
+    int gamesPlayed;
+    public Player() {
+        userScore = 0;
+        correctGuesses = 0;
+        gamesPlayed = 0;
+    }
+    public Player(String name) {
+        userName = name;
+        userScore = 0;
+        correctGuesses = 0;
+        gamesPlayed = 0;
+    }
+
+    // ADDERS
+    public void addUserScore() {
+        userScore += 1;
+    }
+
+    public void addGamesPlayed() {
+        gamesPlayed += 1;
+    }
+
+    public void addCorrectGuesses(int c) {
+        correctGuesses += c;
+    }
+
+    // GETTERS
+    public Integer getCorrectGuesses() {
+        return correctGuesses;
+    }
+    public Integer getUserScore() {
+        return userScore;
+    }
+    public Integer getGamesPlayed() {
+        return gamesPlayed;
+    }
 }
